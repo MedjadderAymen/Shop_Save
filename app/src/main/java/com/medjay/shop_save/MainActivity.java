924282b6419df2ff7ce21573489e2c59a9a970b7
MedjadderAymen <aymen_medjader@hotmail.com>
2020-02-28 21:51:59 +0100

    Initial commit

---
+++ app/src/main/java/com/medjay/shop_save/MainActivity.java
@@ -0,0 +1,14 @@
+package com.medjay.shop_save;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
